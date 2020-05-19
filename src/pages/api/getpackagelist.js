abc728c00fb4049131faada936107104b5050f65
zanisuke <zanisuke@gmail.com>
2020-05-19 22:23:55 +0900

    update getpackagelist

--- src/pages/api/getpackagelist.js
+++ src/pages/api/getpackagelist.js
@@ -1,7 +1,8 @@
-var fs = require("fs")
+const fs = require("fs")
 
 export default (req, res) => {
+  var files = fs.readdirSync('public/packages')
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ name: 'John Doe' }))
+  res.end(JSON.stringify(files))
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,33 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-export default function Index() {
+export default function Index({ json }) {
+    console.log(json)
+    let itemList = json.map((item, key) => {
+        return (
+            <ListItem key={key} button>
+                <ListItemText primary={item} />
+            </ListItem>
+        )
+    })
     return (
         <div>
-            {CreateItemList()}
+            <List>
+                {itemList}
+            </List>
             <img src="images/lain_gray.jpg" />
         </div>
     )
 }
 
-function CreateItemList() {
-    const items = ['apple', 'orange', 'cherry']
-    let itemList = items.map((item, key) => {
-        console.log(key)
+export async function getStaticProps() {
+    const res = await fetch(`http://localhost:3001/api/getpackagelist`)
+    const json = await res.json()
+    return { props: { json, }, }
+}
+
+async function CreatePackageList({ data }) {
+    let itemList = data.map((item, key) => {
         return (
             <ListItem key={key} button>
                 <ListItemText primary={item} />
